6d1f1a3fbe26bb471367d7eab92c0fb00c1df130
29-29 <ackx29sodo@gmail.com>
2024-10-30 02:56:07 +0800

    added courses, student, and other related models

--- src/components/models.ts
+++ src/components/models.ts
@@ -1,8 +1,27 @@
-export interface Todo {
-  id: number;
-  content: string;
+export interface Name {
+  first_name: string
+  middle_name: string
+  last_name: string
 }
 
-export interface Meta {
-  totalCount: number;
+export interface Course {
+  code: string
+  units: number
+  name: string
+}
+
+export interface Faculty {
+  name: Name
+}
+
+export interface CourseSection {
+  course: Course
+  adviser: Faculty
+  sectionCode: string
+}
+
+export interface Student {
+  name: Name
+  institutional_email: string
+  courses: CourseSection[]
 }
