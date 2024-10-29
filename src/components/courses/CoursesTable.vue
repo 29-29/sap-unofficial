70ee41b3f12cd6025d3b6d7a1bd794a97429da69
29-29 <ackx29sodo@gmail.com>
2024-10-30 02:57:01 +0800

    added courses list

---
+++ src/components/courses/CoursesTable.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { isLabeledStatement } from 'typescript';
+import json from './courses.json'
+
+const seed = json.courses
+
+const handleClick = () => {
+  console.log('clicked row!')
+}
+</script>
+
+<template>
+  <div class="q-pa-md">
+    <q-list bordered separator
+    >
+      <q-item v-for="course in json.courses"
+        clickable
+        v-ripple
+        v-on:click="handleClick"
+      >
+        <q-item-section>
+          {{ course.course.code + ' ' + course.sectionCode }}
+        </q-item-section>
+        <q-item-section class="text-right text-caption">
+          {{ course.adviser.name.first_name + ' ' + course.adviser.name.last_name }}
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+
+</template>
--- src/pages/student/CoursesPage.vue
+++ src/pages/student/CoursesPage.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+  import CoursesTable from 'src/components/courses/CoursesTable.vue';
+</script>
+
 <template>
-  <div class="q-pa-lg text-h4">
+  <div class="q-pa-md text-h4 text-bold">
     Courses
   </div>
+  <CoursesTable />
 </template>
