c259301c8a2e98b3244cf69bdaafa39caa09b62c
29-29 <ackx29sodo@gmail.com>
2024-11-01 08:41:34 +0800

    added semester dropdown button

---
+++ src/components/courses/SemesterDropdown.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  // import semesterJson from './semesters.json';
+  import { useCoursesStore } from 'src/store/courses';
+  import { storeToRefs } from 'pinia';
+  import { Semester } from '../models';
+
+  const coursesStore = useCoursesStore();
+  const {semesters, currentSem} = storeToRefs(coursesStore);
+  const currentSemLabel = ref(currentSem.value.semester)
+
+  const selectSemester = (semester: Semester) => {
+    coursesStore.setCurrentSem(semester);
+    currentSemLabel.value = currentSem.value.semester;
+    console.log(semester.semester, currentSem.value.semester, currentSemLabel.value);
+  }
+
+</script>
+
+<template>
+  <div class="q-pa-md">
+    <q-btn-dropdown
+      color="primary"
+      unelevated
+      rounded
+    >
+      <template v-slot:label>
+        <div>
+          <q-badge
+            v-if="currentSemLabel"
+            rounded
+            color="white"
+            class="text-primary"
+          >{{ currentSem.year }}</q-badge>
+          {{ currentSemLabel || 'Semester' }}
+        </div>
+      </template>
+      <q-list
+        separator
+      >
+        <q-item
+          v-for="semester in semesters"
+          clickable
+          v-ripple
+          @click="() => selectSemester(semester)"
+          style="width: 100%;"
+        >
+          <q-item-section>{{ semester.year + ' ' + semester.semester }}</q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
+  </div>
+</template>
--- src/pages/student/CoursesPage.vue
+++ src/pages/student/CoursesPage.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
   import CoursesTable from 'src/components/courses/CoursesTable.vue';
+  import SemesterDropdown from 'src/components/courses/SemesterDropdown.vue';
 </script>
 
 <template>
   <div class="q-pa-md text-h4 text-bold">
     Courses
   </div>
+
+  <SemesterDropdown />
+
   <CoursesTable />
 </template>
