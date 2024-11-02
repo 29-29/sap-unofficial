ab4f9ca1523ba94f840d8caaf8571b722570b159
29-29 <ackx29sodo@gmail.com>
2024-11-02 11:25:59 +0800

    updated list of courses display to expansion list

--- src/components/courses/CoursesTable.vue
+++ src/components/courses/CoursesTable.vue
@@ -1,50 +1,77 @@
 <script setup lang="ts">
-import {  ref } from 'vue';
+import {  reactive, ref } from 'vue';
 // import json from './courses.json';
 import AppointmentDialog from './appointment/AppointmentDialog.vue';
 import CourseSection from '../models';
 import { useCoursesStore } from 'src/store/courses';
 import { storeToRefs } from 'pinia';
 
-// const seed = json.courses;
 const coursesStore = useCoursesStore();
 const {currentSem} = storeToRefs(coursesStore)
 
-const appointmentDialog = ref(false);
-const openAppointmentDialog = () => {
-  appointmentDialog.value = true;
+const dialogIsOpen = ref(false);
+const selectedCourse = ref({} as CourseSection);
+
+const openAppointmentDialog = (course: CourseSection) => {
+  dialogIsOpen.value = true;
+  selectedCourse.value = course;
 };
 
 const closeAppointmentDialog = () => {
-  appointmentDialog.value = false;
+  dialogIsOpen.value = false;
 };
 </script>
 
 <template>
   <div class="q-py-sm">
     <q-list
-      bordered
+    	bordered
       separator
     >
-      <q-item v-for="course in currentSem.courses"
-        clickable
-        v-ripple
-        @click="openAppointmentDialog"
+      <q-expansion-item v-for="course in currentSem.courses"
+        :label="course.course.toString()"
+        group="courses"
       >
-        <q-item-section
-          class="text-h6"
-        >
-          {{ course.course + ' ' + course.sectionCode }}
-        </q-item-section>
-        <q-item-section class="text-right text-bold">
-          {{ course.instructor }}
-        </q-item-section>
-      </q-item>
+        <q-card>
+          <q-card-section>
+            <q-field
+              label="Course"
+              stack-label
+              style="max-width: 300px;"
+            >
+              {{ course.course }}
+            </q-field>
+            <q-field
+              label="Section"
+              stack-label
+              style="max-width: 300px;"
+            >
+              {{ course.sectionCode }}
+            </q-field>
+            <q-field
+              label="Instructor"
+              stack-label
+              style="max-width: 300px;"
+            >
+              {{ course.instructor }}
+            </q-field>
+          </q-card-section>
+          <q-card-actions>
+            <q-btn
+              label="Set appointment"
+              flat
+              text-color="primary"
+              @click="openAppointmentDialog(course)"
+            />
+          </q-card-actions>
+        </q-card>
+      </q-expansion-item>
     </q-list>
   </div>
 
   <AppointmentDialog
-    :activate="appointmentDialog"
+    :activate="dialogIsOpen"
+    :course="selectedCourse"
     @close="closeAppointmentDialog"
   />
 </template>
